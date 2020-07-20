ce71d221cea171a9828129b6131a529e1f068d3e
Amani <amanisanare1@gmail.com>
2020-07-20 03:29:49 +0300

    uploading a project

---
+++ Studentlist.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Student List</title>
+	<style type="text/css">
+		
+		body{
+			background-color: aqua;
+		}
+
+		table{
+			border: 1px solid gray;
+			background-color: white;
+			width: 900px;
+			float: left;
+		}
+
+		th{
+			border: 1px solid gray;
+		}
+
+		td{
+			border: 1px solid gray;
+		}
+
+		#regNo{
+			width: 100PX;
+
+		}
+
+		#name{
+			width: 200PX;
+
+		}
+
+		#programme{
+			width: 400PX;
+
+		}
+
+		#attendance{
+			width: 100PX;
+
+		}
+
+		#image{
+			margin-right: auto;
+			margin-left: auto;
+			display: block;
+		}
+
+		#button{
+			margin-left: 40%;
+		}
+
+		div{
+			float: right;
+		}
+
+		a{
+			text-decoration:none;
+			margin-left: 40%;
+			border: 1px solid gray;
+			border-radius: 10px;
+			padding: 7px;
+			color: black;
+			background: #d6d6c2; 
+		}
+
+
+	</style>
+</head>
+<body>
+	<h1>IS 181 WEB PROGRAMING STUDENT ATTENDANCE SHEET</h1>
+	<table >
+		<thead>
+			
+			<th id="attendance">Attendance</th>
+			<th id="regNo">Reg.No</th>
+			<th id="name">Name</th>
+			<th id="programme">Programme</th>
+
+		</thead>
+		
+		<tbody>
+			<tr>
+				<td><input type="checkbox"></td>
+				<td style="text-align: center;">2019-04-11374</td>
+				<td>Salum,Mrisho A.</td>
+				<td>BSc.in Bussines Information Technology</td>
+			</tr>
+			<tr>
+				<td><input type="checkbox"></td>
+				<td style="text-align: center;">2019-04-11374</td>
+				<td>Salum,Mrisho Abdulrahman.</td>
+				<td>BSc.in Bussines Information Technology</td>
+			</tr>
+			<tr>
+				<td><input type="checkbox"></td>
+				<td style="text-align: center;">2019-04-11374</td>
+				<td>Salum,Mrisho A.</td>
+				<td>BSc.in Bussines Information Technology</td>
+			</tr>
+			<tr>
+				<td><input type="checkbox"></td>
+				<td style="text-align: center;">2019-04-11374</td>
+				<td>Salum,Mrisho A.</td>
+				<td>BSc.in Bussines Information Technology</td>
+			</tr>
+			<tr>
+				<td><input type="checkbox"></td>
+				<td style="text-align: center;">2019-04-11374</td>
+				<td>Salum,Mrisho A.</td>
+				<td>BSc.in Bussines Information Technology</td>
+			</tr>
+			<tr>
+				<td><input type="checkbox"></td>
+				<td style="text-align: center;">2019-04-11374</td>
+				<td>Salum,Mrisho A.</td>
+				<td>BSc.in Bussines Information Technology</td>
+			</tr>
+			<tr>
+				<td><input type="checkbox"></td>
+				<td style="text-align: center;">2019-04-11374</td>
+				<td>Salum,Mrisho A.</td>
+				<td>BSc.in Bussines Information Technology</td>
+			</tr>
+			<tr>
+				<td><input type="checkbox"></td>
+				<td style="text-align: center;">2019-04-11374</td>
+				<td>Salum,Mrisho A.</td>
+				<td>BSc.in Bussines Information Technology</td>
+			</tr><tr>
+				<td><input type="checkbox"></td>
+				<td style="text-align: center;">2019-04-11374</td>
+				<td>Salum,Mrisho A.</td>
+				<td>BSc.in Bussines Information Technology</td>
+			</tr>
+			<tr>
+				<td><input type="checkbox"></td>
+				<td style="text-align: center;">2019-04-11374</td>
+				<td>Salum,Mrisho A.</td>
+				<td>BSc.in Bussines Information Technology</td>
+			</tr>
+			<tr>
+				<td><input type="checkbox"></td>
+				<td style="text-align: center;">2019-04-11374</td>
+				<td>Salum,Mrisho A.</td>
+				<td>BSc.in Bussines Information Technology</td>
+			</tr>
+			<tr>
+				<td><input type="checkbox"></td>
+				<td style="text-align: center;">2019-04-11374</td>
+				<td>Salum,Mrisho A.</td>
+				<td>BSc.in Bussines Information Technology</td>
+			</tr>
+		</tbody>
+			
+		</tbody>
+	</table>
+
+	<div>
+		<p><h4>STUDENT PROFILE:</h4></p>
+		<img id="image" src="sample.png">
+		<p><strong>NAME:</strong> Salum,Mrisho A.</p>
+		<p><strong>REG.No:</strong> 2019-04-11374</p>
+		<p><strong>PROGRAMME:</strong>BSc.in Bussines Information Technology</p>
+		<!-- <button id="button">Attended</button> -->
+		<p><strong><a href="">Attended</a></strong></p>
+	</div>
+
+</body>
+</html>
